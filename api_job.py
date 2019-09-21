9224732f37af885cc3d8817663b8b7995afd616b
dsf19960424 <53976553+dsf19960424@users.noreply.github.com>
2019-09-21 14:17:55 +0800

    Update api_job.py

--- api_job.py
+++ api_job.py
@@ -1,7 +1,7 @@
 import requests
 
 # 获取登录接口
-login_url = 'http://127.0.0.1/admin.php?m=mgr/admin.chklogin&ajax=1'
+login_url = 'http://192.168.1.123/admin.php?m=mgr/admin.chklogin&ajax=1'
 # 登录数据
 lgn_data = {
     'username': 'admin',
@@ -20,7 +20,7 @@ phpid2 = phpid_list[1].split(', ')[1]
 
 
 # 获取添加教师接口
-add_teacher_url = 'http://localhost/admin.php?m=mgr/member2.saveMemberInfo&id='
+add_teacher_url = 'http://192.168.1.123/admin.php?m=mgr/member2.saveMemberInfo&id='
 
 headers = {
     'Cookie': '{}'.format(phpid2)
@@ -47,5 +47,3 @@ teacher_data = {
 
 # 发送添加教师请求
 r2 = requests.post(url=add_teacher_url, data=teacher_data, headers=headers)
-
-print(r2.text)
